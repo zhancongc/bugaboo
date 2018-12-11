157d8b601d6685abbb29fcb05689d99e210cced5
root <root@debug010000002015.local.alipay.net>
2018-12-11 15:05:50 +0800

    bug fixed and upload uwsgi_config.ini

--- server/app/package.py
+++ server/app/package.py
@@ -5,7 +5,7 @@ from app import app
 
 def wxlogin(code):
     appid = app.config.get('APP_ID')
-    with open('../appsecret.txt', 'r') as f:
+    with open('appsecret.txt', 'r') as f:
         app_secret = f.readline().strip()
     url = 'https://api.weixin.qq.com/sns/jscode2session'
     data = {'js_code': code, 'secret': app_secret, 'grant_type': 'authorization_code',
