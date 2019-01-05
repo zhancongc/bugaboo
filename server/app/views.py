c30cd01d506b0b6109a2f2cb0ec57a4819d23b17
詹聪聪 <1227753320@qq.com>
2019-01-05 18:03:25 +0800

    bug fix

--- server/app/views.py
+++ server/app/views.py
@@ -1004,9 +1004,9 @@ def online_service():
         access_token = get_access_token()
         if access_token:
             response_url = 'https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=' + access_token
-            res = requests.post(url=response_url, data=json.dumps(response_data, ensure_ascii=False))
+            res = requests.post(url=response_url, data=json.dumps(response_data, ensure_ascii=False).encode())
             print(res.text)
-            return 'good luck'
+            return ''
         else:
             return 'bad news'
 
